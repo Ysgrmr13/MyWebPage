8843243271f178a78a7500bbfca9090a36c41b16
Ysgrmr <ysgrmr13@gmail.com>
2023-03-06 12:52:58 +0300

    before work

--- portfolioApp/models.py
+++ portfolioApp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
-    descript = models.CharField(max_length=300)
+    title = models.CharField(max_length=22)
+    descript = models.CharField(max_length=100)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
 
