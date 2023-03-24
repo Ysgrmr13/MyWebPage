e1020e3b55f256d1fb06ff10cce3064bd2222628
Ysgrmr <ysgrmr13@gmail.com>
2023-03-24 15:13:42 +0300

    New foto and change my blog

--- portfolioApp/models.py
+++ portfolioApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=22)
     descript = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='portfolio/images/',help_text='image size : 3508x2480')
+    image = models.ImageField(upload_to='portfolio/images/',help_text='image size : 1366x768')
     url = models.URLField(blank=True)
 
     def __str__(self):
